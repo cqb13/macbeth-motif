0750eced950412c5e48646a4602cbc7301824d37
cqb13 <cqb13.main@gmail.com>
2023-05-26 09:50:09 -0400

    Paragraph 2 and fix Paragraph 3

--- components/essay.tsx
+++ components/essay.tsx
@@ -1,23 +1,14 @@
 const Essay = () => {
-  //TODO: When a quote is navigated too, it should be highlighted in the essay
-
   return (
     <article className="text-white">
-      <span className="block indent-5 leading-loose">
-        Guilt can have many different effects on a person, but most often it
-        affects a person's mental health. Guilt's effect on the human mind is
-        what Shakespeare is demonstrating in his play Macbeth, through the
-        character Macbeth, as he goes on a murderous rampage over the course of
-        the play.
-      </span>
       <span className="block indent-5 leading-loose">
         Even before Macbeth's first murder, the manifestation of guilt in the
         mind is made clear by the First Witch, while she tells her story of
         tormenting a sailor, {" "}
-        <div className="indent-0 inline-block duration-150 rounded-md" id="q1">
+        <span className="duration-150 rounded-md" id="q1">
           “Though his bark cannot be lost, / Yet it shall be tempest-tossed.”
           (1.3.24-25).
-        </div>{" "}
+        </span>{" "}
         The reference to the individual's "bark" (referring to their external
         appearance) being tempest-tossed reflects the internal storm of guilt.
         Despite the appearance of stability, the individual's mind is in
@@ -30,6 +21,85 @@ const Essay = () => {
         This is similar to what Macbeth will go through, as while he will look
         fine physically, he will have a very hard time sleeping, and will slowly
         go insane from seeing ghosts of the people he has killed.
+      </span>
+      <span className="block indent-5 leading-loose">
+        Guilt's effect on Macbeth's mind becomes clear after He
+        kills King Duncan, he immediately begins to torment himself over the
+        murder. While on his way to meet up with Lady Macbeth, he begins to hear
+        a voice, and he recounts what it says to Lady Macbeth.{" "}
+        <span className="duration-150 rounded-md" id="q2">
+          “‘Sleep no more!’ to all the house; ‘Glamis hath murdered sleep, and
+          therefore Cawdor shall sleep no more, Macbeth shall sleep no more.’”
+          (2.2.44-46).
+        </span>{" "}
+        This voice is not real, only Macbeth can hear it, the voice crying
+        “Sleep no more”, is a manifestation of the guilt that Macbeth is
+        feeling, he is so guilty that he is punishing himself for what he has
+        done to the king, he is hallucinating a voice telling him that he will
+        no longer be able to sleep for his actions. This moment is depicted in
+        my image “No Sleep”. In the center of the frame is Macbeth, and all
+        around him, is more of himself, at night, wide awake in various forms of
+        torment from his lack of sleep. Macbeth confirms the effects that guilt
+        is having on his mental state, in a conversation with Lady Macbeth,{" "}
+        <span className="duration-150 rounded-md" id="q3">
+          “Ere we will eat our meal in fear and sleep / In the affliction of
+          these terrible dreams” (3.2.18-19).
+        </span>{" "}
+        Macbeth is expressing fear and tormenting his feelings, he and Lady
+        Macbeth have just killed Duncan, and Macbeth has just given the order to
+        kill Banquo, his best friend. He is now always afraid that people may
+        learn of the murder he committed, and his dreams are filled with the
+        horrors of his murders. The fear and torment that comes with guilt is
+        made clear in my image “Torment of the Mind”, it demonstrates this by
+        showing Macbeth and Lady Macbeth in the top right, with scared somber
+        expressing, and around them are symbols for what they have done,
+        Banquo's skull, and Lady Macbeth standing over the dead Duncan in his
+        bed, while she is hiding the murder, those parts of the college are
+        filled with a red tone, and surrounded by fire to show the evil in the
+        moment, and Macbeth and Lady Macbeth are in gray, to show the torment
+        and fear that they are experiencing.
+      </span>
+      <span className="block indent-5 leading-loose">
+        In the end, Guilt forces Macbeth to go into denial, after the feast,
+        Macbeth opens up about his troubles to Lady Macbeth. He has just seen
+        Banquo’s ghost, and sends everyone away after he has a bit of breakdown
+        during the feast, he is then confronted by Lady Macbeth about his
+        actions that night, to whom he says, {" "}
+        <span className="duration-150 rounded-md" id="q4">
+          “Come, we'll to sleep. My strange and self-abuse / Is the initiate
+          fear that wants hard use: / We are yet but young in deed”
+          (3.4.144-145).
+        </span>{" "}
+        Macbeth's guilt is causing him to go into denial from his terrible
+        actions. He knows it was wrong murdering Banquo, and now he is
+        subconsciously punishing himself for it, by having Banquo's ghost
+        torment him. But despite all of these visions, he does not accept that
+        his actions were wrong, instead he says that he should sleep, showing
+        that he wants to hide from his guilt and regret, and hoping that it will
+        go away in time. I show this in my image “Strange Visions”, by having
+        Macbeth, in the center of the image, holding his head in, and facing the
+        floor, with an expression of torment on his face, while he is surrounded
+        by ghosts. My image shows how Macbeth is trying to hide from his
+        actions. Once again, Macbeth's guilt forces him to go into denial of the
+        reality of his situation, when he is being told prophecies from the
+        apparitions that the witches summon, he is told earlier in the play,
+        that Banquo’s children will be King after him, but he is told by the
+        apparition that he can't be killed by anyone born of a women. Macbeth
+        chooses to ignore the obvious contradiction between these two
+        prophecies, and says {" "}
+        <span className="indent-0 duration-150 rounded-md" id="q5">
+          “Sleep in spite of thunder” (4.1.108).
+        </span>{" "}
+        Macbeth is saying that despite everything that is predicted to happen to
+        him, and everything that he has already done, he will ignore it all and
+        “sleep”. I show this in my college, by showing Macbeth in the center of
+        the frame sleeping. Below Macbeth is Banquo, from the movie dead on the
+        train, and to the left of that is Banquo dead from the play. Above
+        Macbeth is Duncan, killed in his sleep, to the left of Duncan, is
+        Macduff, who is not dead, but who Macbeth wants to kill. The entire
+        image is tinted red, to show the evil of Macbeth's actions, and Macbeth
+        sleeping represents him trying to ignore everything evil that he has
+        done.
       </span>
     </article>
   );
--- lib/data/ImageSlides.ts
+++ lib/data/ImageSlides.ts
@@ -23,7 +23,7 @@ const ImageSlides: ImageSlide[] = [
       '“Still it cried "Sleep no more!" to all the house; "Glamis hath murdered sleep, and therefore Cawdor shall sleep no more, Macbeth shall sleep no more.”',
     location: "2.2, 44-46",
     image: "/NoSleep.jpg",
-    inText: ""
+    inText: "q2"
   },
   {
     name: "Torment of the Mind",
@@ -31,7 +31,7 @@ const ImageSlides: ImageSlide[] = [
       "“Ere we will eat our meal in fear and sleep In the affliction of these terrible dreams.”",
     location: "3.2, 18-19",
     image: "/TormentOfTheMind.jpg",
-    inText: ""
+    inText: "q3"
   },
   {
     name: "Strange Visions",
@@ -39,7 +39,7 @@ const ImageSlides: ImageSlide[] = [
       "“Come, we'll to sleep. My strange and self-abuse Is the initiate fear that wants hard use: We are yet but young in deed.”",
     location: "3.4, 144-145",
     image: "/StrangeVisions.jpg",
-    inText: ""
+    inText: "q4"
   },
   {
     name: "Prophecy of the Witches",
@@ -47,7 +47,7 @@ const ImageSlides: ImageSlide[] = [
       "“Sleep in spite of thunder.”",
     location: "4.1, 108",
     image: "/ProphecyOfTheWitches.jpg",
-    inText: ""
+    inText: "q5"
   }
 ];
 
